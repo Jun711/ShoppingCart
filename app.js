1bde72da77cc6fac3b44cfc9383ff15dab19b5b9
Jun <cygoh@umich.edu>
2017-02-07 00:00:35 -0800

    added bootsrapCDN for icons

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressHbs = require('express-handlerbars');
+var expressHbs = require('express-handlebars');
 
 var index = require('./routes/index');
 // var users = require('./routes/users');
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
-app.enginer('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // uncomment after placing your favicon in /public
