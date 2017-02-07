15acae0b6cb7f8b416924d7f091330fea4c595cb
Jun <cygoh@umich.edu>
2017-02-07 03:24:02 -0800

    done outputting data by loading data from mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+
+	Product.find(function(err, docs) {
+		var productChunks = [];
+		var chunkSize = 3;
+		for (var i = 0; i < docs.length; i += chunkSize) {
+			productChunks.push(docs.slice(i, i + chunkSize));
+		}
+		
+		res.render('shop/index', { title: 'Shopping Cart', products: productChunks});
+	});
+	// because of async find, we will get the wrong number of products which are mongoose objects
+	// var products = Product.find();
+	// res.render('shop/index', { title: 'Shopping Cart', products: products});
 });
 
 module.exports = router;
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -15,21 +15,29 @@ var products = [
 		imagePath: 'http://img2.game-oldies.com/sites/default/files/packshots/nintendo-game-boy-advance/dragon-ball-z-supersonic-warriors-usa.png',
 		title: 'Dragon Ball',
 		description: 'Goku vs Vegeta',
-		price: 10
+		price: 12
 	}),
 	new Product({
 		imagePath: 'http://vignette1.wikia.nocookie.net/residentevilfanfiction/images/7/71/RE7.jpg/revision/latest?cb=20150324221456',
 		title: 'Resident Evil 7',
 		description: 'Alice rules',
-		price: 10
+		price: 15
 	}),
 	new Product({
 		imagePath: 'https://pbs.twimg.com/profile_images/571024672750178304/GpGC8aTW.jpeg',
 		title: 'League of Legends',
 		description: '5 v 5!',
-		price: 10
+		price: 17
+	}),
+	new Product({
+		imagePath: 'https://pbs.twimg.com/profile_images/808475349671493632/nvi7WJf4.jpg',
+		title: 'Dota 2',
+		description: 'Defense',
+		price: 19
 	})
 ];
+
+//products.remove({});
 
 var done = 0;
 for (var i = 0; i < products.length; i++) {
